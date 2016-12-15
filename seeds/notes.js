86c416e7d7f0cf3d918554f58f629efcd0877406
AnnaUlyanova <a.ulyanova89@gmail.com>
2016-12-16 09:33:24 +1300

    t

--- seeds/notes.js
+++ seeds/notes.js
@@ -5,7 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('notes').insert({id:1, comments: 'Really Great I like it', book_id:1})
+        knex('notes').insert({id:1, comments: 'Really Great I like it', book_id:2}),
+        knex('notes').insert({id:2, comments: 'You should learn how to write', book_id:3}),
+        knex('notes').insert({id:3, comments: 'One more episode please, I can\'t wait to see it colapsing', book_id:4}),
+        knex('notes').insert({id:4, comments: 'The author was on crack and cocaine', book_id:20}),
+        knex('notes').insert({id:5, comments: 'He was also a pedophile', book_id:20})
       ]);
     });
 };
